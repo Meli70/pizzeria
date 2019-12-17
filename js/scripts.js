7689bdcef67eb03c69a8e504206b2721645f4658
Meli70 <cynthiameli142@gmail.com>
2019-12-17 11:03:30 +0300

    Modify scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+class Pizza {
+  constructor(size, crust, toppings, quantity) {
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.quantity = quantity;
+  }
+}
+
+    
